7ae14349c3ce5d3350dead00ef23e9ac628b8c80
Nabil Ulil Albab <nabilulilalbab78@gmail.com>
2026-03-25 04:59:51 +0700

    Make API port and host dynamic based on environment variables

--- detik-dynamic-scraper/src/api/main.py
+++ detik-dynamic-scraper/src/api/main.py
@@ -237,4 +237,6 @@ async def get_statistics():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    host = os.getenv("API_HOST", "0.0.0.0")
+    port = int(os.getenv("API_PORT", 8000))
+    uvicorn.run(app, host=host, port=port)
